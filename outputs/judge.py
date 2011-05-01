75962ea12314e128cf9795a6fb24102a0a12517d
Mark Chapman <chapman@cs.wisc.edu>
2011-05-01 13:17:43 -0500

    sample test directories: ./waldo.py -l s -u b , script to run common options, current outputs

--- outputs/judge.py
+++ outputs/judge.py
@@ -11,7 +11,7 @@ from sys import argv
 if __name__ == '__main__' :
     for f in argv[1:] :
         with open( f ) as fin :
-            mceq = [ 0, 0, 0, 0 ]
+            (m, c, e, q) = (0, 0, 0, 0)
             for line in fin :
                 i = line.find( '<->' )
                 if i >= 0 :
@@ -21,27 +21,22 @@ if __name__ == '__main__' :
                     fl1 = line.find( '.', i + 5 ) + 1
                     fl2 = line.find( ' ', fl1 )
                     fl = line[fl1:fl2]
-                    a1 = line.find( '/testavl' ) >= 0
-                    a2 = line.find( '/avltree', i ) >= 0
-                    s1 = line.find( '/teststkl' ) >= 0
-                    s2 = line.find( '/stackli', i ) >= 0
-                    t1 = line.find( '/testtree' ) >= 0
-                    t2 = line.find( '/tree', i ) >= 0
-                    if (a1 and a2) or (s1 and s2) or (t1 and t2) :
-                        if (fe == fl) :
-                            mceq[0] += 1
-                        else :
-                            mceq[1] += 1
-                    elif (fe == fl) :
-                        mceq[1] += 1
-                    else :
-                        if (a1 or s1 or t1) and (a2 or s2 or t2) :
-                            mceq[2] += 1
-                            print '  error',
-                        else :
-                            mceq[3] += 1
-                            print '  ?????',
-                        print ': %s.%s != %s.%s' % (
-                            'testavl' if a1 else ('teststkl' if s1 else ('testtree' if t1 else '?')), fe,
-                            'avltree' if a2 else ('stackli' if s2 else ('tree' if t2 else '?')), fl )
-            print '%s:  %d  %d  %d  %d  ...  %f%%' % (f, mceq[0], mceq[1], mceq[2], mceq[3], 100.0*mceq[0]/(sum(mceq)))
+                    (ta, at) = ('/testavl', '/avltree')
+                    (a1, a2, a3) = (line.find(ta) >= 0, line.find(ta,i) >= 0, line.find(at,i) >= 0)
+                    (ts, sl) = ('/teststkl', '/stackli')
+                    (s1, s2, s3) = (line.find(ts) >= 0, line.find(ts,i) >= 0, line.find(sl,i) >= 0)
+                    (tt, tr) = ('/testtree', '/tree')
+                    (t1, t2, t3) = (line.find(tt) >= 0, line.find(tt,i) >= 0, line.find(tr,i) >= 0)
+                    if (a1 and (a2 or a3)) or (s1 and (s2 or s3)) or (t1 and (t2 or t3)) :
+                        if (fe == fl) : m += 1
+                        else : c += 1
+                    elif (fe == fl) : c += 1
+                    elif (a1 or s1 or t1) and (a2 or a3 or s2 or s3 or t2 or t3) :
+                        e += 1
+                        print '  error: {}.{} != {}.{}'.format(ta if a1 else (ts if s1 else (tt if t1 else '?')), fe,
+                            ta if a2 else (ts if s2 else (tt if t2 else (at if a3 else (sl if s3 else (tr if t3 else
+                            '?'))))), fl )
+                    else : q += 1
+            print '{}:  {}  {}  {} '.format( f, m, c, e ),
+            if q == 0 : print '-- prc: {:.2%}, rcl: {:.2%}'.format( 0 if m == 0 else float(m) / (m + e), m / 406.0 )
+            else : print ' {}'.format( q )
